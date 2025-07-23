b13cfa6e1787e44352173cf52601a4fde8515953
liangzid <2273067585@qq.com>
2025-07-23 21:39:54 +0800

    update.

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ echo "Using `bash run.sh` to run this file."
 pip3 install torch tqdm
 
 
-nohup python main.py > stress_test_results &
+nohup python main.py > stress_test_results.log &
 
 
 echo "Process is now on the background."
